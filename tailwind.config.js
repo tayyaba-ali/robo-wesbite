879062bc9d617941edde8b552b7b537f0e005bb6
Tayyaba Ali <98578632+tayyaba-ali@users.noreply.github.com>
2023-04-28 22:24:05 +0500

    cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,32 @@ module.exports = {
 			center: true,
 			padding: '1rem',
 		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1200px',
+		},
 		extend: {
 			colors: {
 				'Robo-darkBlue': '#161735',
 				'Robo-lightBlue': '#1b6ef0',
 				'Robo-blue': '#00d0f5',
+				'Robo-gray': '#2e2e2e',
+				'gradient-right': '#237ece',
+				'gradient-left': '#051b88',
 			},
-			backgroundImage: {
-				'Robo-bg': 'linear-gradient(to right, #237ece, #051b88',
+			boxShadow: {
+				'robo-shadow': '0 0 20px 3px rgba(0, 0, 0, 0.05)',
 			},
 		},
 		fontFamily: {
 			Saira: 'Saira,Manrope',
+			Manrope:"Manrope"
 		},
 	},
 
 	plugins: [],
 };
 
+;
